4962adb0b28103ed1bea123f6f4a4b136ec652c1
vakhno <vakhnohere@gmail.com>
2020-11-15 21:47:19 +0200

    Layout was created

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import './App.css'
+import Header from './components/Header'
+import Balance from './components/Balance'
+import AddTransaction from './components/AddTransaction'
+import IncomeList from './components/IncomeList'
+import ExpenseList from './components/ExpenseList'
+function App() {
+	return (
+		<div className="container">
+			<div className="app-wrapper">
+				<Header />
+				<Balance />
+				<AddTransaction />
+				<IncomeList />
+				<ExpenseList />
+			</div>
+		</div>
+	)
+}
+
+export default App
