dd85629b8656ff98c5934fbef119a1710f90807c
vakhno <vakhnohere@gmail.com>
2020-11-18 23:53:41 +0200

    Added total incomes and expenses

--- src/components/Balance.js
+++ src/components/Balance.js
@@ -1,6 +1,19 @@
-import React from 'react'
-
+import React, { useContext } from 'react'
+import { GlobalContext, GlobalCOntext } from '../context/GlobalContext'
 const Balance = () => {
+	const { incomeTransactions, expenseTransactions } = useContext(GlobalContext)
+
+	const incomeAmounts = incomeTransactions.map(
+		transaction => transaction.incomeAmount
+	)
+
+	const expenseAmounts = expenseTransactions.map(
+		transaction => transaction.expenseAmount
+	)
+
+	const totalIncome = incomeAmounts.reduce((acc, elem) => (acc += elem), 0).toFixed(2)
+	const totalExpense = expenseAmounts.reduce((acc, elem) => (acc += elem), 0).toFixed(2)
+
 	return (
 		<div className='balance'>
 			<h2>Your Balance</h2>
@@ -8,11 +21,11 @@ const Balance = () => {
 			<div className="income-expense">
 				<div className="plus">
 					<h3>Income</h3>
-					<p>+$0.00</p>
+					<p>+${totalIncome}</p>
 				</div>
 				<div className="minus">
 					<h3>Exprenses</h3>
-					<p>-$0.00</p>
+					<p>-${totalExpense}</p>
 				</div>
 			</div>
 		</div>
